32ae9f899abcb235934666d744b0974b8fe48aff
mqtran <minh-quan.tran@cacd2.fr>
2021-09-06 17:44:34 +0200

    add CORS config

---
+++ src/main/java/com/cacf/cdt/bffclone/configuration/CorsGlobalConfiguration.java
@@ -0,0 +1,17 @@
+package com.cacf.cdt.bffclone.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .maxAge(3600);
+    }
+}
