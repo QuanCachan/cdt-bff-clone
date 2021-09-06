be0a69a9a1adbc2ab7de1aaf87b0fbcb3e479fd3
mqtran <minh-quan.tran@cacd2.fr>
2021-09-06 23:55:42 +0200

    update repositories

--- src/main/java/com/cacf/cdt/bffclone/repository/CDTUserRepository.java
+++ src/main/java/com/cacf/cdt/bffclone/repository/CDTUserRepository.java
@@ -10,11 +10,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CDTUserRepository extends CrudRepository<CDTUser, String> {
-    @Query("select distinct new com.cacf.cdt.bffclone.repository.vo.TupleVO(f.agencyRegion, f.agency) from IDDFile f")
-    List<TupleVO<String, String>> findAgencies();
 
     @Query("select distinct a from CDTUser a join IDDFile f on f.advisor.number = a.number where a.role = :role and f.agency = :agency order by a.lastName, a.firstName")
     List<CDTUser> findAllByAgencyAndRole(@Param("agency") String agency, @Param("role") CDTUserRole role);
+
+    @Query("select distinct new com.cacf.cdt.bffclone.repository.vo.TupleVO(f.agencyRegion, f.agency) from IDDFile f")
+    List<TupleVO<String, String>> findAgencies();
 
     default List<CDTUser> findAllAdvisorsByAgency(String agency) {
         return findAllByAgencyAndRole(agency, CDTUserRole.ADVISOR);
