2a8730bba5e6fc737f73cee9cf958f77b7097ef4
mqtran <minh-quan.tran@cacd2.fr>
2021-09-06 14:50:57 +0200

    initialization

---
+++ src/test/java/com/cacf/cdt/bffclone/BffCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacf.cdt.bffclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
